3099bdf4526020ff7d66d5e0f932f2ab53581652
Judy-Yeboah <jyeboah513@gmail.com>
2023-10-31 06:10:00 +0000

    Sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("object-oriented programming with Python")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
