fa8f79e2ec625b57100e0e31fd1079007e0a22e5
Judy-Yeboah <jyeboah513@gmail.com>
2023-11-16 01:44:12 +0000

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     argLen = len(sys.argv)
-    if argLen == 2:
-        print("{} argument.".format(argLen - 1))
+    if argLen == 0:
+        print("{} arguments.".format(argLen - 1))
     elif argLen == 1:
-        print("{} arguments:".format(argLen - 1))
+        print("{} argument:".format(argLen - 1))
     else:
         print("{} arguments:".format(argLen - 1))
     for i in range(1, argLen):
