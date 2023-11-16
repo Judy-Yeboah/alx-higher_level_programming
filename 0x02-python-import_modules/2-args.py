da552e140947587dc9b342bbe5c53fc906c155a2
Judy-Yeboah <jyeboah513@gmail.com>
2023-11-16 01:30:20 +0000

    dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argLen = len(sys.argv)
+    if argLen == 2:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 1:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
+    for iterator in range(1, argLen):
+        print("{}: {}".format(iterator, sys.argv[iterator]))
